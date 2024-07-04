70dba2273ca59b0f9c81a7dc97f0f1bb20123702
DulajUmansha <dulajumansha@gmail.com>
2024-07-04 18:46:16 +0530

    Add Image Uploader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ImageUpload = () => {
+  const [file, setFile] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [uploadedImageName, setUploadedImageName] = useState('');
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+      const response = await fetch('http://localhost:8000/api/upload/', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setUploadedImage(result.image_base64);
+        setUploadedImageName(result.number_plate);
+      } else {
+        console.error('Upload failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
+      {uploadedImage && (
+        <div>
+          <p>Uploaded Image: {uploadedImageName}</p>
+          <img src={uploadedImage} alt="Uploaded" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default ImageUpload;
